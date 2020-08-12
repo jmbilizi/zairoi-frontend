b1e28bc186c0e54c8c69511b7fc35a2ae89eed58
Admin <janviermbilizi@gmail.com>
2020-08-12 11:38:42 -0400

    set admin route

--- src/auth/AdminRoute.js
+++ src/auth/AdminRoute.js
@@ -1,23 +1,24 @@
-import React, { Component } from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import { isAuth } from './helpers';
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import { isAuth } from "./helpers";
 
 const AdminRoute = ({ component: Component, ...rest }) => (
-    <Route
-        {...rest}
-        render={props =>
-            isAuth() && isAuth().role === 'admin' ? (
-                <Component {...props} />
-            ) : (
-                <Redirect
-                    to={{
-                        pathname: '/signin',
-                        state: { from: props.location }
-                    }}
-                />
-            )
-        }
-    ></Route>
+  // props means components passed down to this private route component
+  <Route
+    {...rest}
+    render={(props) =>
+      isAuth() ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{
+            pathname: "/signin",
+            state: { from: props.location },
+          }}
+        />
+      )
+    }
+  />
 );
 
 export default AdminRoute;
