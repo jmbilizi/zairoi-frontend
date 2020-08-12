9e99447e35beb74f7f98d798df3cdfe02ca58530
Admin <janviermbilizi@gmail.com>
2020-08-12 08:18:27 -0400

    edited the authentication file and admin identication

--- src/admin/Admin.js
+++ src/admin/Admin.js
@@ -1,48 +1,48 @@
 import React, { Component } from "react";
 import Posts from "../post/Posts";
 import Users from "../user/Users";
-import { isAuthenticated } from "../auth";
+import { isAuth } from "../auth/helpers";
 import { Redirect } from "react-router-dom";
 
 class Admin extends Component {
-    state = {
-        redirectToHome: false
-    };
+  state = {
+    redirectToHome: false,
+  };
 
-    componentDidMount() {
-        if (isAuthenticated().user.role !== "admin") {
-            this.setState({ redirectToHome: true });
-        }
+  componentDidMount() {
+    if (isAuth().role !== "admin") {
+      this.setState({ redirectToHome: true });
+    }
+  }
+
+  render() {
+    if (this.state.redirectToHome) {
+      return <Redirect to="/" />;
     }
 
-    render() {
-        if (this.state.redirectToHome) {
-            return <Redirect to="/" />;
-        }
-
-        return (
-            <div>
-                <div className="jumbotron">
-                    <h2>Admin Dashboard</h2>
-                    <p className="lead">Welcome to React Frontend</p>
-                </div>
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <h2>Posts</h2>
-                            <hr />
-                            <Posts />
-                        </div>
-                        <div className="col-md-6">
-                            <h2>Users</h2>
-                            <hr />
-                            <Users />
-                        </div>
-                    </div>
-                </div>
+    return (
+      <div>
+        <div className="jumbotron">
+          <h2>Admin Dashboard</h2>
+          <p className="lead">Welcome to Zairoi.com</p>
+        </div>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-6">
+              <h2>Posts</h2>
+              <hr />
+              <Posts />
             </div>
-        );
-    }
+            <div className="col-md-6">
+              <h2>Users</h2>
+              <hr />
+              <Users />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Admin;
