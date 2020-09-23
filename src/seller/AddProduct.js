50b02dcc0e47ba2dedcc63d5b36e75f18c4ecf9b
Admin <janviermbilizi@gmail.com>
2020-09-23 11:28:16 -0400

    added title on add product form

--- src/seller/AddProduct.js
+++ src/seller/AddProduct.js
@@ -87,58 +87,82 @@ const AddProduct = () => {
     };
 
     const newPostForm = () => (
-        <form className="mb-3" onSubmit={clickSubmit}>
-            <h4>Post Photo</h4>
-            <div className="form-group">
-                <label className="btn btn-secondary">
-                    <input onChange={handleChange('photo')} type="file" name="photo" accept="image/*" />
-                </label>
-            </div>
+      <form className="mb-3" onSubmit={clickSubmit}>
+        <label className="text-muted">Product photo</label>
+        <div className="form-group">
+          <label className="btn btn-secondary">
+            <input
+              onChange={handleChange("photo")}
+              type="file"
+              name="photo"
+              accept="image/*"
+            />
+          </label>
+        </div>
 
-            <div className="form-group">
-                <label className="text-muted">Name</label>
-                <input onChange={handleChange('name')} type="text" className="form-control" value={name} />
-            </div>
+        <div className="form-group">
+          <label className="text-muted">Name</label>
+          <input
+            onChange={handleChange("name")}
+            type="text"
+            className="form-control"
+            value={name}
+          />
+        </div>
 
-            <div className="form-group">
-                <label className="text-muted">Description</label>
-                <textarea onChange={handleChange('description')} className="form-control" value={description} />
-            </div>
+        <div className="form-group">
+          <label className="text-muted">Description</label>
+          <textarea
+            onChange={handleChange("description")}
+            className="form-control"
+            value={description}
+          />
+        </div>
 
-            <div className="form-group">
-                <label className="text-muted">Price</label>
-                <input onChange={handleChange('price')} type="number" className="form-control" value={price} />
-            </div>
+        <div className="form-group">
+          <label className="text-muted">Price</label>
+          <input
+            onChange={handleChange("price")}
+            type="number"
+            className="form-control"
+            value={price}
+          />
+        </div>
 
-            <div className="form-group">
-                <label className="text-muted">Category</label>
-                <select onChange={handleChange('category')} className="form-control">
-                    <option>Please select</option>
-                    {categories &&
-                        categories.map((c, i) => (
-                            <option key={i} value={c._id}>
-                                {c.name}
-                            </option>
-                        ))}
-                </select>
-            </div>
+        <div className="form-group">
+          <label className="text-muted">Category</label>
+          <select onChange={handleChange("category")} className="form-control">
+            <option>Please select</option>
+            {categories &&
+              categories.map((c, i) => (
+                <option key={i} value={c._id}>
+                  {c.name}
+                </option>
+              ))}
+          </select>
+        </div>
 
-            <div className="form-group">
-                <label className="text-muted">Shipping</label>
-                <select onChange={handleChange('shipping')} className="form-control">
-                    <option>Please select</option>
-                    <option value="0">No</option>
-                    <option value="1">Yes</option>
-                </select>
-            </div>
+        <div className="form-group">
+          <label className="text-muted">Shipping</label>
+          <select onChange={handleChange("shipping")} className="form-control">
+            <option>Please select</option>
+            <option value="0">No</option>
+            <option value="1">Yes</option>
+          </select>
+        </div>
 
-            <div className="form-group">
-                <label className="text-muted">Quantity</label>
-                <input onChange={handleChange('quantity')} type="number" className="form-control" value={quantity} />
-            </div>
+        <div className="form-group">
+          <label className="text-muted">Quantity</label>
+          <input
+            onChange={handleChange("quantity")}
+            type="number"
+            className="form-control"
+            value={quantity}
+          />
+        </div>
 
-            <button className="btn btn-outline-primary">Create Product</button>
-        </form>
+        <button className="btn btn-outline-primary">Create Product</button>
+      </form>
     );
 
     const showError = () => (
@@ -161,16 +185,17 @@ const AddProduct = () => {
         );
 
     return (
-        <Layout title="Add a new product" description={`G'day ${user.name}, ready to add a new product?`}>
-            <div className="row">
-                <div className="col-md-8 offset-md-2">
-                    {showLoading()}
-                    {showSuccess()}
-                    {showError()}
-                    {newPostForm()}
-                </div>
-            </div>
-        </Layout>
+      <Layout>
+        <div className="row">
+          <div className="col-md-8 offset-md-2">
+            <h2 className="mt-5 mb-5">Create a new product</h2>
+            {showLoading()}
+            {showSuccess()}
+            {showError()}
+            {newPostForm()}
+          </div>
+        </div>
+      </Layout>
     );
 };
 
