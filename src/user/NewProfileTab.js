64ba8d94271057c2d30bdb812c41fc8c65a9fc0c
Admin <janviermbilizi@gmail.com>
2020-10-08 22:10:33 -0400

    Deleted the tap icons

--- src/user/NewProfileTab.js
+++ src/user/NewProfileTab.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from "react";
 import { Link } from "react-router-dom";
 import DefaultProfile from "../images/avatar.jpg";
-import { isAuth, getCookie } from "../auth/helpers";
+import { isAuth } from "../auth/helpers";
 
 import SinglePost from "../post/SinglePost";
 // import antd from "antd";
@@ -81,140 +81,6 @@ const NewProfileTab = ({ user, following, followers, posts }) => {
     </>
   );
 
-  const postsTab = () => (
-    <>
-      {`${posts.length}     `}
-      <i
-        className="fab fa-usps"
-        style={{
-          fontSize: "25px",
-          position: "relative",
-          height: "50px",
-        }}
-      >
-        <span
-          style={{
-            position: "absolute",
-            height: "20px",
-            bottom: "0",
-            right: "0",
-            left: "0",
-            fontSize: "20px",
-            width: "200px",
-          }}
-        >
-          <small>Posts</small>
-        </span>
-      </i>
-    </>
-  );
-  const followersTab = () => (
-    <>
-      {`${followers.length} `}
-      <i
-        className="fas fa-user-friends"
-        style={{
-          fontSize: "25px",
-          position: "relative",
-          height: "50px",
-        }}
-      >
-        <span
-          style={{
-            position: "absolute",
-            height: "20px",
-            bottom: "0",
-            right: "0",
-            left: "0",
-            fontSize: "20px",
-          }}
-        >
-          <small>lowers</small>
-        </span>
-      </i>
-    </>
-  );
-
-  const followingTab = () => (
-    <>
-      {`${following.length}              `}
-      <i
-        className="fas fa-user-friends"
-        style={{
-          fontSize: "25px",
-          position: "relative",
-          height: "50px",
-        }}
-      >
-        <span
-          style={{
-            position: "absolute",
-            height: "20px",
-            bottom: "0",
-            right: "0",
-            left: "0",
-            fontSize: "20px",
-          }}
-        >
-          <small>lowing</small>
-        </span>
-      </i>
-    </>
-  );
-  const shopTab = () => (
-    <>
-      <i
-        className="fas fa-store"
-        style={{
-          fontSize: "25px",
-          position: "relative",
-          height: "50px",
-        }}
-      >
-        <span
-          style={{
-            position: "absolute",
-            height: "20px",
-            bottom: "0",
-            right: "0",
-            left: "0",
-            fontSize: "20px",
-          }}
-        >
-          <small>Shop</small>
-        </span>
-      </i>
-    </>
-  );
-
-  const manageUserShopTab = () => (
-    <>
-      <i
-        className="fas fa-edit"
-        style={{
-          fontSize: "25px",
-          position: "relative",
-          height: "50px",
-        }}
-      >
-        <i className="fas fa-trash"></i>
-        <i className="fas fa-store"></i>
-        <span
-          style={{
-            position: "absolute",
-            height: "20px",
-            bottom: "0",
-            right: "0",
-            left: "0",
-            fontSize: "20px",
-          }}
-        >
-          <small>MG Products</small>
-        </span>
-      </i>
-    </>
-  );
-
   const { TabPane } = Tabs;
 
   function callback(key) {
@@ -223,22 +89,22 @@ const NewProfileTab = ({ user, following, followers, posts }) => {
 
   const ProfileTabs = () => (
     <Tabs defaultActiveKey="1" onChange={callback} centered>
-      <TabPane tab={postsTab()} key="1">
+      <TabPane tab={`${posts.length} Posts`} key="1">
         <div className="row">
           <div className="col-sm-12">{allPosts()}</div>
         </div>
       </TabPane>
-      <TabPane tab={followersTab()} key="2">
-        <div className="row">{allFollowers()}</div>
-      </TabPane>
-      <TabPane tab={followingTab()} key="3">
-        <div className="row">{allFollowing()}</div>
-      </TabPane>
-      <TabPane tab={shopTab()} key="4">
+      <TabPane tab="Shop" key="4">
         <div className="row">{<ShopByUser userId={user._id} />}</div>
       </TabPane>
+      <TabPane tab={`${followers.length} Followers`} key="2">
+        <div className="row">{allFollowers()}</div>
+      </TabPane>
+      <TabPane tab={`${following.length} Following`} key="3">
+        <div className="row">{allFollowing()}</div>
+      </TabPane>
       {isAuth() && (isAuth()._id === user._id || isAuth().role === "admin") ? (
-        <TabPane tab={manageUserShopTab()} key="5">
+        <TabPane tab="MG Products" key="5">
           {<ManagerProductsByUser userId={user._id} />}
         </TabPane>
       ) : (
