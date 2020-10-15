d6ff318ee5ecb8f9dc15d44d42b0fa0cce9914ab
Admin <janviermbilizi@gmail.com>
2020-10-15 18:54:44 -0400

    played with img view

--- src/post/EditPost.js
+++ src/post/EditPost.js
@@ -3,6 +3,7 @@ import { singlePost, update } from "./apiPost";
 import { isAuth, getCookie } from "../auth/helpers";
 import { Redirect } from "react-router-dom";
 import Layout from "../core/Layout";
+import DefaultPost from "../images/mountains.jpg";
 
 class EditPost extends Component {
   constructor() {
@@ -11,7 +12,6 @@ class EditPost extends Component {
       id: "",
       title: "",
       body: "",
-      postedBy: "",
       redirectToProfile: false,
       error: "",
       fileSize: 0,
@@ -25,8 +25,7 @@ class EditPost extends Component {
         this.setState({ redirectToProfile: true });
       } else {
         this.setState({
-          postedBy: data._id,
-          id: data.postedBy._id,
+          id: data._id,
           title: data.title,
           body: data.body,
           error: "",
@@ -73,7 +72,6 @@ class EditPost extends Component {
     if (this.isValid()) {
       const postId = this.props.match.params.postId;
       const token = getCookie("token");
-      // const token = isAuth().token;
 
       update(postId, token, this.postData).then((data) => {
         if (data.error) this.setState({ error: data.error });
@@ -127,24 +125,22 @@ class EditPost extends Component {
   );
 
   render() {
-    const {
-      postedBy,
-      id,
-      title,
-      body,
-      redirectToProfile,
-      error,
-      loading,
-    } = this.state;
+    const { id, title, body, redirectToProfile, error, loading } = this.state;
+    console.log(id);
 
     if (redirectToProfile) {
-      return <Redirect to={`/post/${postedBy}`} />;
+      return <Redirect to={`/user/${isAuth._id}`} />;
     }
+    const photoUrl = id
+      ? `${
+          process.env.REACT_APP_API_URL
+        }/post/photo/${id}?${new Date().getTime()}`
+      : DefaultPost;
 
     return (
       <Layout>
         <div className="container">
-          <h2 className="mt-5 mb-5">{title}</h2>
+          <h2 className="mt-5 mb-3">{title}</h2>
 
           <div
             className="alert alert-danger"
@@ -164,10 +160,8 @@ class EditPost extends Component {
           <img
             style={{ height: "200px", width: "auto" }}
             className="img-thumbnail"
-            src={`${
-              process.env.REACT_APP_API_URL
-            }/post/photo/${postedBy}?${new Date().getTime()}`}
-            onError={(i) => (i.target.style.display = "none")}
+            src={photoUrl}
+            // onError={(i) => (i.target.src = `${DefaultPost}`)}
             alt={title}
           />
 
@@ -181,3 +175,181 @@ class EditPost extends Component {
 }
 
 export default EditPost;
+
+// class EditPost extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       id: "",
+//       title: "",
+//       body: "",
+//       postedBy: "",
+//       redirectToProfile: false,
+//       error: "",
+//       fileSize: 0,
+//       loading: false,
+//     };
+//   }
+
+//   init = (postId) => {
+//     singlePost(postId).then((data) => {
+//       if (data.error) {
+//         this.setState({ redirectToProfile: true });
+//       } else {
+//         this.setState({
+//           postedBy: data._id,
+//           id: data.postedBy._id,
+//           title: data.title,
+//           body: data.body,
+//           error: "",
+//         });
+//       }
+//     });
+//   };
+
+//   componentDidMount() {
+//     this.postData = new FormData();
+//     const postId = this.props.match.params.postId;
+//     this.init(postId);
+//   }
+
+//   isValid = () => {
+//     const { title, body, fileSize } = this.state;
+//     if (fileSize > 1000000) {
+//       this.setState({
+//         error: "File size should be less than 100kb",
+//         loading: false,
+//       });
+//       return false;
+//     }
+//     if (title.length === 0 || body.length === 0) {
+//       this.setState({ error: "All fields are required", loading: false });
+//       return false;
+//     }
+//     return true;
+//   };
+
+//   handleChange = (name) => (event) => {
+//     this.setState({ error: "" });
+//     const value = name === "photo" ? event.target.files[0] : event.target.value;
+
+//     const fileSize = name === "photo" ? event.target.files[0].size : 0;
+//     this.postData.set(name, value);
+//     this.setState({ [name]: value, fileSize });
+//   };
+
+//   clickSubmit = (event) => {
+//     event.preventDefault();
+//     this.setState({ loading: true });
+
+//     if (this.isValid()) {
+//       const postId = this.props.match.params.postId;
+//       const token = getCookie("token");
+//       // const token = isAuth().token;
+
+//       update(postId, token, this.postData).then((data) => {
+//         if (data.error) this.setState({ error: data.error });
+//         else {
+//           this.setState({
+//             loading: false,
+//             title: "",
+//             body: "",
+//             redirectToProfile: true,
+//           });
+//         }
+//       });
+//     }
+//   };
+
+//   editPostForm = (title, body) => (
+//     <form>
+//       <div className="form-group">
+//         <label className="text-muted">Post Photo</label>
+//         <input
+//           onChange={this.handleChange("photo")}
+//           type="file"
+//           accept="image/*"
+//           className="form-control"
+//         />
+//       </div>
+//       <div className="form-group">
+//         <label className="text-muted">Title</label>
+//         <input
+//           onChange={this.handleChange("title")}
+//           type="text"
+//           className="form-control"
+//           value={title}
+//         />
+//       </div>
+
+//       <div className="form-group">
+//         <label className="text-muted">Body</label>
+//         <textarea
+//           onChange={this.handleChange("body")}
+//           type="text"
+//           className="form-control"
+//           value={body}
+//         />
+//       </div>
+
+//       <button onClick={this.clickSubmit} className="btn btn-raised btn-primary">
+//         Update Post
+//       </button>
+//     </form>
+//   );
+
+//   render() {
+//     const {
+//       postedBy,
+//       id,
+//       title,
+//       body,
+//       redirectToProfile,
+//       error,
+//       loading,
+//     } = this.state;
+
+//     if (redirectToProfile) {
+//       return <Redirect to={`/post/${postedBy}`} />;
+//     }
+
+//     return (
+//       <Layout>
+//         <div className="container">
+//           <h2 className="mt-5 mb-5">{title}</h2>
+
+//           <div
+//             className="alert alert-danger"
+//             style={{ display: error ? "" : "none" }}
+//           >
+//             {error}
+//           </div>
+
+//           {loading ? (
+//             <div className="jumbotron text-center">
+//               <h2>Loading...</h2>
+//             </div>
+//           ) : (
+//             ""
+//           )}
+
+//           <img
+//             style={{ height: "200px", width: "auto" }}
+//             className="img-thumbnail"
+//             src={`${
+//               process.env.REACT_APP_API_URL
+//             }/post/photo/${postedBy}?${new Date().getTime()}`}
+//             onError={(i) => (i.target.style.display = "none")}
+//             alt={title}
+//           />
+
+//           {isAuth().role === "admin" && this.editPostForm(title, body)}
+
+//           {isAuth()._id === id && this.editPostForm(title, body)}
+//         </div>
+//       </Layout>
+//     );
+//   }
+// }
+
+// export default EditPost;
