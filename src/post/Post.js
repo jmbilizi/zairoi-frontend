6d06aeaae8ccee779342a6da8590718ba95daf2f
Admin <janviermbilizi@gmail.com>
2020-10-18 18:28:09 -0400

    Changed to functional component

--- src/post/Post.js
+++ src/post/Post.js
@@ -1,63 +1,52 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import { list } from "./apiPost";
 import { isAuth } from "../auth/helpers";
 
 import Layout from "../core/Layout";
 import SinglePost from "./SinglePost";
 
-class Post extends Component {
-  constructor() {
-    super();
-    this.state = {
-      posts: [],
-      page: 1,
-    };
-  }
+const Post = () => {
+  const [posts, setPosts] = useState([]);
+  const [page, setPage] = useState(1);
 
-  loadPosts = (page) => {
+  const loadPosts = (page) => {
     list(page).then((data) => {
       if (data.error) {
         console.log(data.error);
       } else {
-        this.setState({
-          posts: data,
-        });
+        setPosts(data);
       }
     });
   };
 
-  componentDidMount() {
-    this.loadPosts(this.state.page);
-  }
+  useEffect(() => {
+    loadPosts(page);
+  }, []);
 
-  render() {
-    const { posts } = this.state;
-    console.log(posts);
-    const following = isAuth().following;
+  const following = isAuth().following;
 
-    return (
-      <Layout>
-        <div className="row">
-          <div className="col-md-2 col-sm-0"></div>
-          <div className="col-md-8 col-sm-12">
-            {posts
-              .filter(
-                (post) =>
-                  following.includes(post.postedBy._id) === true ||
-                  isAuth()._id === post.postedBy._id ||
-                  post.postedBy.role === "admin"
-              )
-              .map((post, i) => (
-                <div key={i}>
-                  <SinglePost postId={post._id} />
-                </div>
-              ))}
-          </div>
-          <div className="col-md-2 col-sm-0"></div>
+  return (
+    <Layout>
+      <div className="row">
+        <div className="col-md-2 col-sm-0"></div>
+        <div className="col-md-8 col-sm-12">
+          {posts
+            .filter(
+              (post) =>
+                following.includes(post.postedBy._id) === true ||
+                isAuth()._id === post.postedBy._id ||
+                post.postedBy.role === "admin"
+            )
+            .map((post, i) => (
+              <div key={i}>
+                <SinglePost postId={post._id} />
+              </div>
+            ))}
         </div>
-      </Layout>
-    );
-  }
-}
+        <div className="col-md-2 col-sm-0"></div>
+      </div>
+    </Layout>
+  );
+};
 
 export default Post;
