6e704416d08617bd2f09fca94c7587e38018d9dd
Admin <janviermbilizi@gmail.com>
2020-10-30 18:10:31 -0400

    moved modal files in the chat

--- src/chat/ChatBox.js
+++ src/chat/ChatBox.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Modal from "../Modal";
+import Modal from "./Modal";
 import Message from "./Message";
 
 function ToggleModal({ id, name }) {
