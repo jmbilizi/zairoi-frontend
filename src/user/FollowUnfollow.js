5afe3eb7a752f160c91f385582b04a485aeb2fa6
Admin <janviermbilizi@gmail.com>
2020-11-03 02:11:17 -0500

    created followUnfollow button

---
+++ src/user/FollowUnfollow.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import { follow, unfollow } from "./apiUser";
+import { isAuth, getCookie } from "../auth/helpers";
+
+const FollowUnfollow = ({ user }) => {
+  const [following, setFollowing] = useState(() => {
+    const match = user.followers.find((follower) => {
+      // one id has many other ids (followers) and vice versa
+      return follower === isAuth()._id;
+    });
+    return match
+  });
+
+  const token = getCookie("token");
+  const userId = isAuth()._id;
+
+  const followClick = () => {
+    follow(userId, token, user._id).then((data) => {
+      if (data.error) {
+        console.log("error");
+      } else {
+        setFollowing(true);
+      }
+    });
+  };
+
+  const unfollowClick = () => {
+    unfollow(userId, token, user._id).then((data) => {
+      if (data.error) {
+        console.log("error");
+      } else {
+        setFollowing(false);
+      }
+    });
+  };
+
+  return (
+    <div className="d-inline-block">
+      {!following ? (
+        <button onClick={followClick} className="btn btn-success btn-raised">
+          Follow
+        </button>
+      ) : (
+        <button onClick={unfollowClick} className="btn btn-warning btn-raised">
+          UnFollow
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default FollowUnfollow;
