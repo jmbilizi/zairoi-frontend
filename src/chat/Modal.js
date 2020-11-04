b15eab3f23862b6921f4948443e841aa2b6339ad
Admin <janviermbilizi@gmail.com>
2020-11-03 23:42:08 -0500

    changed to functional components

--- src/chat/Modal.js
+++ src/chat/Modal.js
@@ -1,29 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createPortal } from "react-dom";
 import "./Modal.css";
 // We get hold of the div with the id modal that we have created in index.html
 const modalRoot = document.getElementById("modal");
-class Modal extends React.Component {
-  constructor(props) {
-    super(props);
-    // We create an element div for this modal
-    this.element = document.createElement("div");
-    this.element.setAttribute("class", "my-modal");
-    //e.setAttribute("class", `saveBtn${time[i]}`);
-  }
-  // We append the created div to the div#modal
-  componentDidMount() {
-    modalRoot.appendChild(this.element);
-  }
-  /**
-   * We remove the created div when this Modal Component is unmounted
-   * Used to clean up the memory to avoid memory leak
-   */
-  componentWillUnmount() {
-    modalRoot.removeChild(this.element);
-  }
-  render() {
-    return createPortal(this.props.children, this.element);
-  }
-}
+const Modal = (props) => {
+  // We create an element div for this modal
+  let element = document.createElement("div");
+  element.setAttribute("class", "my-modal");
+
+  useEffect(() => {
+    //mount
+    modalRoot.appendChild(element);
+    //unmount
+    return () => {
+      modalRoot.removeChild(element);
+    };
+  });
+
+  return createPortal(props.children, element);
+};
 export default Modal;
