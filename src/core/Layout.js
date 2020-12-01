83a2daca90c1fddd1772afbe4177f8764ddfd380
Admin <janviermbilizi@gmail.com>
2020-11-30 20:41:14 -0500

    added data target on navbar

--- src/core/Layout.js
+++ src/core/Layout.js
@@ -16,51 +16,31 @@ const Layout = ({ children, match, history }) => {
   };
 
   const nav = () => (
-    <ul
-      style={{
-        listStyleType: "none",
-        opacity: "100%",
-        margin: "0",
-        background: "",
-      }}
-      className="navbar nav-tabs sticky-top"
-    >
-      <li className="nav-item">
-        <Link to="/" className="nav-link" style={isActive("/")}>
-          <i
-            className="fas fa-house-user"
-            style={{
-              fontSize: "25px",
-              position: "relative",
-              height: "50px",
-            }}
-          >
-            <span
-              style={{
-                position: "absolute",
-                height: "20px",
-                bottom: "0",
-                right: "0",
-                left: "0",
-                fontSize: "20px",
-              }}
-            >
-              <small>Home</small>
-            </span>
-          </i>
-        </Link>
-      </li>
-      {!isAuth() && (
-        <Fragment>
+    <nav className="navbar navbar-expand-lg navbar-light nav-tabs sticky-top">
+      <a className="navbar-brand" href="/">
+        Logo
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarText"
+        aria-controls="navbarText"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarText">
+        <ul style={{ listStyleType: "none" }} className="navbar-nav ml-auto">
           <li className="nav-item">
-            <Link to="/signin" className="nav-link" style={isActive("/signin")}>
+            <Link to="/" className="nav-link" style={isActive("/")}>
               <i
-                className="fas fa-sign-in-alt"
+                className="fas fa-house-user"
                 style={{
                   fontSize: "25px",
                   position: "relative",
                   height: "50px",
-                  top: "0",
                 }}
               >
                 <span
@@ -73,78 +53,244 @@ const Layout = ({ children, match, history }) => {
                     fontSize: "20px",
                   }}
                 >
-                  <small> In</small>
+                  <small>Home</small>
                 </span>
               </i>
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/signup" className="nav-link" style={isActive("/signup")}>
-              <i
-                className="fas fa-user-plus"
-                style={{
-                  fontSize: "25px",
-                  position: "relative",
-                  height: "50px",
-                  top: "0",
-                }}
-              >
-                <span
-                  style={{
-                    position: "absolute",
-                    height: "20px",
-                    bottom: "0",
-                    right: "0",
-                    left: "0",
-                    fontSize: "20px",
-                  }}
+          {!isAuth() && (
+            <Fragment>
+              <li className="nav-item">
+                <Link
+                  to="/signin"
+                  className="nav-link"
+                  style={isActive("/signin")}
                 >
-                  <small>Up</small>
-                </span>
-              </i>
-            </Link>
-          </li>
-        </Fragment>
-      )}
+                  <i
+                    className="fas fa-sign-in-alt"
+                    style={{
+                      fontSize: "25px",
+                      position: "relative",
+                      height: "50px",
+                      top: "0",
+                    }}
+                  >
+                    <span
+                      style={{
+                        position: "absolute",
+                        height: "20px",
+                        bottom: "0",
+                        right: "0",
+                        left: "0",
+                        fontSize: "20px",
+                      }}
+                    >
+                      <small> In</small>
+                    </span>
+                  </i>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/signup"
+                  className="nav-link"
+                  style={isActive("/signup")}
+                >
+                  <i
+                    className="fas fa-user-plus"
+                    style={{
+                      fontSize: "25px",
+                      position: "relative",
+                      height: "50px",
+                      top: "0",
+                    }}
+                  >
+                    <span
+                      style={{
+                        position: "absolute",
+                        height: "20px",
+                        bottom: "0",
+                        right: "0",
+                        left: "0",
+                        fontSize: "20px",
+                      }}
+                    >
+                      <small>Up</small>
+                    </span>
+                  </i>
+                </Link>
+              </li>
+            </Fragment>
+          )}
 
-      {isAuth() && isAuth().role === "admin" && (
-        <li className="nav-item">
-          <Link className="nav-link" style={isActive("/admin")} to="/admin">
-            Admin
-          </Link>
-        </li>
-      )}
-      {isAuth() && (
-        <Fragment>
+          {isAuth() && isAuth().role === "admin" && (
+            <li className="nav-item">
+              <Link className="nav-link" style={isActive("/admin")} to="/admin">
+                Admin
+              </Link>
+            </li>
+          )}
+          {isAuth() && (
+            <Fragment>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  style={isActive("/posts")}
+                  to="/posts"
+                >
+                  Posts
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" style={isActive("/shop")} to="/shop">
+                  Shop
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  style={isActive(`/findpeople`)}
+                  to={`/findpeople`}
+                >
+                  Find People
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/post/create"
+                  style={isActive("/post/create")}
+                  className="nav-link"
+                >
+                  <i
+                    style={{ position: "relative", height: "50px" }}
+                    className="fas fa-2x fa-plus"
+                  >
+                    <span
+                      style={{
+                        position: "absolute",
+                        height: "20px",
+                        bottom: "0",
+                        right: "0",
+                        left: "0",
+                        fontSize: "20px",
+                      }}
+                    >
+                      <small>Post</small>
+                    </span>
+                  </i>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  style={isActive(`/messaging`)}
+                  to={`/messaging`}
+                >
+                  <i
+                    className="far fa-envelope"
+                    style={{
+                      fontSize: "25px",
+                      position: "relative",
+                      height: "50px",
+                    }}
+                  >
+                    <span
+                      style={{
+                        position: "absolute",
+                        height: "20px",
+                        bottom: "0",
+                        right: "0",
+                        left: "0",
+                        fontSize: "20px",
+                      }}
+                    >
+                      <small>Msg</small>
+                    </span>
+                  </i>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  className="nav-link"
+                  style={isActive(`/user/${isAuth()._id}`)}
+                  to={`/user/${isAuth()._id}`}
+                >
+                  <div style={{ position: "relative", height: "50px" }}>
+                    <img
+                      style={{
+                        borderRadius: "50%",
+                        // border: "1px solid black",
+                      }}
+                      // className="float-left mr-2"
+                      height="30px"
+                      width="30px"
+                      onError={(i) => (i.target.src = `${DefaultProfile}`)}
+                      src={`${process.env.REACT_APP_API_URL}/user/photo/${
+                        isAuth()._id
+                      }`}
+                      alt={isAuth().name}
+                    />
+                    <span
+                      style={{
+                        position: "absolute",
+                        height: "22px",
+                        bottom: "0",
+                        right: "0",
+                        left: "0",
+                        fontSize: "20px",
+                      }}
+                    >
+                      <small>Me</small>
+                    </span>
+                  </div>
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <span
+                  className="nav-link"
+                  style={{ cursor: "pointer" }}
+                  onClick={() => {
+                    signout(() => {
+                      history.push("/");
+                    });
+                  }}
+                >
+                  <i
+                    className="fas fa-sign-out-alt"
+                    style={{
+                      fontSize: "25px",
+                      position: "relative",
+                      height: "50px",
+                    }}
+                  >
+                    <span
+                      style={{
+                        position: "absolute",
+                        height: "20px",
+                        bottom: "0",
+                        right: "0",
+                        left: "0",
+                        fontSize: "20px",
+                      }}
+                    >
+                      <small>Out</small>
+                    </span>
+                  </i>
+                </span>
+              </li>
+            </Fragment>
+          )}
           <li className="nav-item">
-            <Link className="nav-link" style={isActive("/posts")} to="/posts">
-              Posts
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link className="nav-link" style={isActive("/shop")} to="/shop">
-              Shop
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              style={isActive(`/findpeople`)}
-              to={`/findpeople`}
-            >
-              Find People
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              to="/post/create"
-              style={isActive("/post/create")}
-              className="nav-link"
-            >
+            <Link className="nav-link" style={isActive("/cart")} to="/cart">
               <i
                 style={{ position: "relative", height: "50px" }}
-                className="fas fa-2x fa-plus"
+                className="fas fa-2x fa-shopping-cart "
               >
+                {" "}
+                <sup>
+                  <small style={{}}>{itemTotal()}</small>
+                </sup>
                 <span
                   style={{
                     position: "absolute",
@@ -155,138 +301,14 @@ const Layout = ({ children, match, history }) => {
                     fontSize: "20px",
                   }}
                 >
-                  <small>Post</small>
+                  <small>Cart</small>
                 </span>
               </i>
             </Link>
           </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              style={isActive(`/messaging`)}
-              to={`/messaging`}
-            >
-              <i
-                className="far fa-envelope"
-                style={{
-                  fontSize: "25px",
-                  position: "relative",
-                  height: "50px",
-                }}
-              >
-                <span
-                  style={{
-                    position: "absolute",
-                    height: "20px",
-                    bottom: "0",
-                    right: "0",
-                    left: "0",
-                    fontSize: "20px",
-                  }}
-                >
-                  <small>Msg</small>
-                </span>
-              </i>
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link
-              className="nav-link"
-              style={isActive(`/user/${isAuth()._id}`)}
-              to={`/user/${isAuth()._id}`}
-            >
-              <div style={{ position: "relative", height: "50px" }}>
-                <img
-                  style={{
-                    borderRadius: "50%",
-                    // border: "1px solid black",
-                  }}
-                  // className="float-left mr-2"
-                  height="30px"
-                  width="30px"
-                  onError={(i) => (i.target.src = `${DefaultProfile}`)}
-                  src={`${process.env.REACT_APP_API_URL}/user/photo/${
-                    isAuth()._id
-                  }`}
-                  alt={isAuth().name}
-                />
-                <span
-                  style={{
-                    position: "absolute",
-                    height: "22px",
-                    bottom: "0",
-                    right: "0",
-                    left: "0",
-                    fontSize: "20px",
-                  }}
-                >
-                  <small>Me</small>
-                </span>
-              </div>
-            </Link>
-          </li>
-
-          <li className="nav-item">
-            <span
-              className="nav-link"
-              style={{ cursor: "pointer" }}
-              onClick={() => {
-                signout(() => {
-                  history.push("/");
-                });
-              }}
-            >
-              <i
-                className="fas fa-sign-out-alt"
-                style={{
-                  fontSize: "25px",
-                  position: "relative",
-                  height: "50px",
-                }}
-              >
-                <span
-                  style={{
-                    position: "absolute",
-                    height: "20px",
-                    bottom: "0",
-                    right: "0",
-                    left: "0",
-                    fontSize: "20px",
-                  }}
-                >
-                  <small>Out</small>
-                </span>
-              </i>
-            </span>
-          </li>
-        </Fragment>
-      )}
-      <li className="nav-item">
-        <Link className="nav-link" style={isActive("/cart")} to="/cart">
-          <i
-            style={{ position: "relative", height: "50px" }}
-            className="fas fa-2x fa-shopping-cart "
-          >
-            {" "}
-            <sup>
-              <small style={{}}>{itemTotal()}</small>
-            </sup>
-            <span
-              style={{
-                position: "absolute",
-                height: "20px",
-                bottom: "0",
-                right: "0",
-                left: "0",
-                fontSize: "20px",
-              }}
-            >
-              <small>Cart</small>
-            </span>
-          </i>
-        </Link>
-      </li>
-    </ul>
+        </ul>
+      </div>
+    </nav>
   );
   const check = () => {
     if (isAuth() && isAuth().length !== -1) {
