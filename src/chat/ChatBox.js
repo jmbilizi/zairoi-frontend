238d4ee5a5e45f97c0c66aa53965984add4ebbf2
Janviermbilizi <janviermbilizi@gmail.com>
2020-12-28 23:45:24 -0500

    return the class

--- src/chat/ChatBox.js
+++ src/chat/ChatBox.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "./Modal";
 import Message from "./Message";
 
-function ToggleModal({ id, name }) {
+function ToggleModal({ id, name, theclass }) {
   const [showModal, setShowModal] = useState(false);
   const [reduceModal, setreduceModal] = useState(false);
 
@@ -22,8 +22,10 @@ function ToggleModal({ id, name }) {
       <div
         //I will add on click event that will send you to a card that allow you to send message to the particular user
         onClick={togglemodal}
+        className={`${theclass}`}
       >
         <i className="far fa-comment-alt"></i> Message
+        {/* {!showModal ? "Message" : "End Chat"} */}
       </div>
       {showModal ? (
         <Modal reducer={reduceModal}>
